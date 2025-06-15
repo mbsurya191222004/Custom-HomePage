5460e73beacc59617d7a438afa5a1e1eb9b80665
Guest User <Guest@admins-iMac-4.local>
2025-06-15 22:12:12 +0530

    fixed links look

--- src/components/QuickAccess.jsx
+++ src/components/QuickAccess.jsx
@@ -4,7 +4,7 @@ function QuickAccess() {
     const [Links,SetLinks]= useState([["ChatGPT","https://chatgpt.com/"],["YouTube","https://www.youtube.com/"]])
     const [Icons,SetIcons] = useState([])
     return (
-        <div className="absolute m-12 p-3 bg-[rgba(0,0,0,0.5)]  rounded-2xl flex  justify-between top-[80%] left-[50%] translate-x-[-50%]">
+        <div className="absolute top-[80%] left-[50%] translate-x-[-50%] flex justify-evenly">
             {
                 Links.map(
                     (link)=>{
@@ -12,11 +12,15 @@ function QuickAccess() {
                         let icon = `https://www.google.com/s2/favicons?domain=${domain}&sz=64`;
                         return (
                             <a href={link[1]}>
-                                <div className="h-20 w-20 text-white">
-                                <img src={icon} alt={link[0]} />
+                            <div className="bg-[rgba(0,0,0,0.2)] p-2 rounded-lg text-white flex flex-col justify-around items-center m-2">
+                                
+                                <img height="50px" width="50px" src={icon} alt={link[0]} />
                                 <p>{link[0]}</p>
-                                </div>
+                            
+                            
+                            </div>
                             </a>
+                            
                         )
                     }
                 )
