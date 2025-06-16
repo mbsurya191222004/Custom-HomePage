54e12892cda4afe1f50213ea290c9f5485cf7fb7
SURYA <mbsurya19122004@gmail.com>
2025-06-16 20:20:45 +0530

    animations done

---
+++ src/components/Video.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { motion } from 'motion/react'
+
+export default function  Video() {
+  return (
+    <>
+        <motion.video 
+        initial = {{
+            opacity : 0, 
+            scale : 1.5,
+            background : "red"
+        }}
+        animate = {{
+            
+            opacity : 100,
+            scale : 1,
+            transition : {
+                duration : 1
+            }
+        }}
+        src="BG2.mp4" autoPlay muted loop/> 
+    </>
+        
+  )
+}
